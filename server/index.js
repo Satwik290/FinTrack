8b94ec330c31a13c9e1f85d065f1a6d4f691d604
satwik <mohanty.satwik290@gmail.com>
2025-08-15 21:56:29 +0530

    Added the transactions  api to main server

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/transactions', require('./routes/transactionRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
